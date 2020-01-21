5f624bd03e92d268812d15cd63030dd51c31e4d1
Andreas Schuderer <schuderer@gmx.de>
2020-01-21 16:46:21 +0100

    Initial structure

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for model."""
+
+__author__ = """{{author_name}}"""
+__email__ = "{{author_email}}"
+__version__ = "{{version}}"
+
---
+++ private/readme.txt
@@ -0,0 +1,2 @@
+This is the location where to put local test data
+and any files that you don't want to be in version control.
