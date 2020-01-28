2a706c4aa525fc09712f9103693e1bf133a4f560
Andreas Schuderer <schuderer@gmx.de>
2020-01-28 16:27:24 +0100

    Build script

---
+++ build.py
@@ -0,0 +1,136 @@
+from glob import glob
+import os
+import shutil
+import platform
+import subprocess
+import sys
+import venv
+
+from zipfile import ZipFile
+import yaml
+
+required_config = {
+    "deploy": {
+        "include": {},
+        "requirements": {
+            "file": {},
+            "platform": {},
+            "save_to": {},
+        },
+    },
+    "api": {
+        "name": {},
+        "version": {},
+    },
+}
+venv_location = ".venv_temp_deploy"
+build_path = "build"
+deployed_config_name = "LAUNCHPAD_CFG.yml"
+deployed_requirements_name = "LAUNCHPAD_REQ.txt"
+
+
+def delete_dir(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def validate_config(config_dict, required, path=""):
+    for item in required:
+        path_start = (path + ":") if path else ""
+        if item not in config_dict:
+            raise ValueError("Missing key in config file: {}".format(path_start + item))
+        validate_config(config_dict[item], required[item], path_start + item)
+
+
+def get_requirements(req_file, target_platform, target_dir):
+    try:
+        # Check whether requirement versions are pinned
+        with open(req_file) as f:
+            req_str = f.read()
+            if '==' not in req_str:
+                print("\nWARNING: The requirements file specified in your config\n"
+                      "does not seem to specify versions. It is highly recommended to only\n"
+                      "deploy version-specific requirements (a.k.a. pinned requirements)\n"
+                      "to ensure reproducibility even as pypi packages change.\n"
+                      "Ideally, you would explicitly manage your requirements,\n"
+                      "or create a frozen requirements file in a clean environment.\n"
+                      "For the latter, see https://github.com/schuderer/mllaunchpad/issues/60\n")
+                print("Do you REALLY want to continue with unpinned requirements?")
+                yes = input("(Type 'yes' to continue, Enter to quit)")
+                if yes != "yes":
+                    sys.exit(0)
+
+        print(" Creating temporary environment {}...".format(venv_location))
+        venv.create(venv_location, clear=True, with_pip=True)
+
+        interpreter = os.path.join(
+            venv_location,
+            "Scripts" if platform.system() == "Windows" else "bin",
+            "python"
+        )
+
+        print(" Downloading requirements from {} for platform {}...".format(req_file, target_platform))
+        delete_dir(target_dir)
+        create_dir(target_dir)
+        cmd = [interpreter, "-m", "pip",  "download",
+               "--platform", target_platform, "--no-deps", "-d", target_dir, "-r", req_file]
+        print(" ".join(cmd))
+        subprocess.Popen(cmd).wait()
+
+        return req_str
+    finally:
+        delete_dir(venv_location)
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise ValueError("Expected config file as only command line argument.")
+    config_file = sys.argv[1]
+
+    with open(config_file) as f:
+        config_str = f.read()
+        config = yaml.safe_load(config_str)
+        validate_config(config, required_config)
+
+    root_path = os.path.dirname(config_file)
+    old_working_dir = os.getcwd()
+    os.chdir(os.path.abspath(root_path))
+
+    print("Collecting requirements...")
+    req_cfg = config["deploy"]["requirements"]
+    req_str = get_requirements(req_cfg["file"], req_cfg["platform"], req_cfg["save_to"])
+
+    files = []
+    for file_pattern in config["deploy"]["include"]:
+        expanded_files = glob(file_pattern, recursive=True)
+        filtered_files = []
+        for f in expanded_files:
+            unwanted = [e for e in config["deploy"]["exclude"] if e in f]
+            if len(unwanted) == 0:
+                filtered_files.append(f)
+        files.extend(filtered_files)
+
+    delete_dir(build_path)
+    create_dir(build_path)
+    zip_name = os.path.join(build_path, "{}_{}.zip".format(config["api"]["name"], config["api"]["version"]))
+    print("Packaging zip file {}...".format(zip_name))
+    with ZipFile(zip_name, 'w') as zip_file:
+        for file in files:
+            print("Adding file {}".format(file))
+            zip_file.write(file)
+        print("Adding file {}".format(deployed_config_name))
+        zip_file.writestr(deployed_config_name, config_str)
+        print("Adding file {}".format(deployed_requirements_name))
+        zip_file.writestr(deployed_requirements_name, req_str)
+
+    os.chdir(old_working_dir)
+    print("\nDone. Build artifacts can be found in the '{}' subdirectory.".format(build_path))
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
